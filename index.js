ff7954b906002199e17613098cac4e5573c74b41
TonyMM55 <tonymmoran@gmail.com>
2023-07-20 20:59:06 +0100

    added vite to fix CORS issues

--- index.js
+++
@@ -1,8 +0,0 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value;
-    
-    console.log(inputValue);
-})
---
+++ vite-project/index.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-7c902-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+
+    push(shoppingListDB, inputValue)
+
+    clearInputFieldEl()
+
+    appendItemToShoppingListEl(inputValue)
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
