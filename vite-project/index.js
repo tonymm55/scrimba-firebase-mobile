5e9c66ca82a81942edf5102849809de632b3ce13
TonyMM55 <tonymmoran@gmail.com>
2023-07-30 11:35:12 +0100

    added public folder for vite purposes

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-7c902-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,7 +7,7 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListDB = ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -16,17 +16,51 @@ const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
-    push(shoppingListDB, inputValue)
+    push(shoppingListInDB, inputValue)
 
     clearInputFieldEl()
-
-    appendItemToShoppingListEl(inputValue)
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+
+    if (snapshot.exists()) {
+      let itemsArray = Object.entries(snapshot.val())
+
+      clearShoppingListEl()
+
+      for (let i = 0; i < itemsArray.length; i++) {
+          let currentItem = itemsArray[i]
+          let currentItemId = currentItem[0]
+          let currentItemValue = currentItem[1]
+
+          appendItemToShoppingListEl(currentItem)
+      }
+    } else {
+        shoppingListEl.innerHTML = "No items here yet"
+    }
+});
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
 }
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,5 +1,11 @@
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-	plugins: []	
+	plugins: [],
+	server: {
+		static: "Assets",
+	},	
+	build: {
+		assetsDir: 'public',
+	},
 })
